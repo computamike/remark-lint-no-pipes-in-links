fff731caecb794b30f4aaf6eeb9647e6af2d2ae3
Mike Hingley <computa_mike@hotmail.com>
2020-11-23 02:06:45 +0000

    updating fileoverview / readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # remark-link-escape
+Warn when a link has a pipe within a link.
+see [free-programming-books issue 5176](https://github.com/EbookFoundation/free-programming-books/issues/5176) 
+ 
+  ## Example
+  ###### In
+   ```markdown
+   [This is a link | with a pipe](https://unifiedjs.com/)
+   ```
+  
+  ###### Out
+
+   ```text
+    1:1  warning  link contains an unescaped |              link-escape       remark-lint
+   ```
+ 
+  ###### In
+   ```markdown
+   [This is a link /| with a pipe](https://unifiedjs.com/)
+   ```
+  
+  ###### Out
+
+   ```text
+    no messages
+   ```
--- index.js
+++ index.js
@@ -4,7 +4,32 @@
  * @license MIT
  * @module remark-link-escape
  * @fileoverview
- *   Warn when a link has a pipe within a link.
+ *  Warn when a link has a pipe within a link.
+ *  
+ *  see [free-programming-books issue 5176](https://github.com/EbookFoundation/free-programming-books/issues/5176) 
+ * 
+ *  ## Example
+ *  ###### In
+ *   ```markdown
+ *   [This is a link | with a pipe](https://unifiedjs.com/)
+ *   ```
+ *  
+ *  ###### Out
+ *
+ *   ```text
+ *    1:1  warning  link contains an unescaped |              link-escape       remark-lint
+ *   ```
+ * 
+ *  ###### In
+ *   ```markdown
+ *   [This is a link /| with a pipe](https://unifiedjs.com/)
+ *   ```
+ *  
+ *  ###### Out
+ *
+ *   ```text
+ *    no messages
+ *   ```
  */
 
 'use strict'
